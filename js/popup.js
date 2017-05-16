e0960efb79a4580ff86512bd39ae17a151057a5e
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-16 03:14:30 -0400

    popup created

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,30 @@
 ﻿'use strict';
 
 let Popup = (function() {
+	let _status = JSON.parse(localStorage._status || "{}"),
+		_started = JSON.parse(localStorage._started || "false"),
+		_panelStart = $("#start-panel"),
+		_panelStop = $("#stop-panel"),
+		_btnStart = $("#start"),
+		_btnStop = $("#stop");
+
+	let showStartPanel = () => {
+		_panelStart.show();
+		_panelStop.hide();
+	};
+
+	let showStopPanel = () => {
+		_panelStart.hide();
+		_panelStop.show();
+	};
+
 	let init = () => {
-		};
+		if (_started) {
+			showStopPanel();
+		} else {
+			showStartPanel();
+		}
+	};
 
 	return {
 		init: init
